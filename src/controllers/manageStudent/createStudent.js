3c7123dbbd17e4a7eb426eac9d82ed51de50c68a
Arpitha <arpithaapoojary@gmail.com>
2025-10-29 10:21:58 +0530

    structured format

--- src/controllers/manageStudent/createStudent.js
+++ src/controllers/manageStudent/createStudent.js
@@ -24,14 +24,23 @@ export default router.post("/", async (req, res) => {
       });
     }
 
-    console.log({ ...req.body });
+    let isEmail = email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+
+    if (!isEmail) {
+      return res.send({
+        message: "email is invalid",
+      });
+    }
+
+    let studentRollno = await StudentModel.findOne({ rollno: rollno });
+    // console.log({ ...req.body });
     // console.log(name);
 
-    await StudentModel.create({
-      name,
-      rollno,
-      email,
-    });
+    // await StudentModel.create({
+    //   name,
+    //   rollno,
+    //   email,
+    // });
 
     return res.send({
       message: "Ok",
