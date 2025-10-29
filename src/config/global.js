5bf83fe58b4b11ccf92ee9ba8df242bdb7b5d245
Arpitha <arpithaapoojary@gmail.com>
2025-10-29 10:33:01 +0530

    structured format

---
+++ src/config/global.js
@@ -0,0 +1,10 @@
+export const RESPONSE = {
+  SUCCESS: {
+    code: 200,
+    message: "Everything worked as expected",
+  },
+  UNKNOWN_ERR: {
+    code: 500,
+    message: "Something went wrong",
+  },
+};
