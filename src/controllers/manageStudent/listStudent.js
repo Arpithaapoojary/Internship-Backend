b0908a60f849784fae913e45656431569d0bac3b
Arpitha <arpithaapoojary@gmail.com>
2025-10-30 10:58:56 +0530

    all changes are applied

--- src/controllers/manageStudent/listStudent.js
+++ src/controllers/manageStudent/listStudent.js
@@ -5,32 +5,38 @@ import { RESPONSE } from "../../config/global.js";
 import { send, setErrMsg } from "../../helper/responseHelper.js";
 const router = Router();
 
-export default router.get("/", async (requestAnimationFrame, res) => {
+export default router.get("/", async (req, res) => {
   try {
-    // let studentData = await StudentModel.find(
+    let student_id = req.query.student_id;
+
+    let query = {
+      isactive: STATE.ACTIVE,
+    };
+
+    student_id != undefined ? (query._id = student_id) : "";
+    let studentData = await StudentModel.find(
+      query,
+
+      {
+        isactive: 0,
+        __v: 0,
+      }
+    );
+
+    // let studentData = await StudentModel.aggregate([
     //   {
-    //     isactive: STATE.ACTIVE,
+    //     $match: {
+    //       isactive: STATE.ACTIVE,
+    //     },
     //   },
+
     //   {
-    //     isactive: 0,
-    //     __v: 0,
-    //   }
-    // );
-
-    let studentData = await StudentModel.aggregate([
-      {
-        $match: {
-          isactive: STATE.ACTIVE,
-        },
-      },
-
-      {
-        $project: {
-          isactive: 0,
-          __v: 0,
-        },
-      },
-    ]);
+    //     $project: {
+    //       isactive: 0,
+    //       __v: 0,
+    //     },
+    //   },
+    // ]);
 
     if (studentData.length == 0) {
       return send(res, setErrMsg(RESPONSE.NOT_FOUND, "students"));
